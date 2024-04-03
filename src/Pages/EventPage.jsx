7b975ce117f1b5e28409fc69e23e8f607251798b
kristl63 <denis.kristlik@unob.cz>
2024-04-03 08:25:47 +0100

    3.4.24 hodina

---
+++ src/Pages/EventPage.jsx
@@ -0,0 +1,30 @@
+// import { useFreshItem } from "@hrbolek/uoisfrontend-shared/src"
+import { useParams } from "react-router-dom"
+import { UserLargeCard } from "../Components"
+import { useFreshItem, CreateAsyncQueryValidator, useDispatch } from "@hrbolek/uoisfrontend-shared/src"
+import { FetchUserByIdAsyncAction } from "../Queries"
+import { FetchEventByIdAsyncAction } from "../Queries/FetchEventByIdAsyncAction copy"
+import { EventLargeCard } from "../Components/Event/EventLargeCard"
+
+const validator = CreateAsyncQueryValidator({error: "Nepovedlo se načíst uživatele", success: "Načtení uživatele se povedlo"})
+export const EventPage = ()  => {
+    const {id} = useParams()
+    const [onResolve, onReject] = validator(useDispatch())
+    const [event, userPromise] = useFreshItem({id}, FetchEventByIdAsyncAction)
+    userPromise.then(onResolve, onReject)
+
+    if (event) {
+        return (
+            //<UserLargeCard user={user} />
+            //<div>Project name here
+            //    {JSON.stringify(event)}
+            //</div>
+            <EventLargeCard event={event}/>
+        )
+    } else {
+        return (
+            <div>Nahravam Udalost...</div>
+        )
+    }
+    
+}
---
+++ src/Queries/FetchEventByIdAsyncAction copy.js
@@ -0,0 +1,20 @@
+import { CreateAsyncActionFromQuery } from "@hrbolek/uoisfrontend-shared/src"
+
+const query = `query ($id: UUID!) {
+    result: eventById(id: $id) {
+
+           __typename
+          id
+          lastchange
+          name
+          startdate
+          enddate
+          eventType{
+            id
+            name
+          }
+        
+      }
+    }`
+
+export const FetchEventByIdAsyncAction = CreateAsyncActionFromQuery(query)
